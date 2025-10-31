0834a2a2e5397c24337cae0c4d7d001a31265526
pachan <pogchamb1@gmail.com>
2025-10-31 17:44:12 +0700

    add wifiscaner class

--- app/src/main/java/com/example/wifi_conection_scaner/core/App.kt
+++ app/src/main/java/com/example/wifi_conection_scaner/core/App.kt
@@ -1,4 +1,8 @@
 package com.example.wifi_conection_scaner.core
 
-class app {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
 }
--- app/src/main/java/com/example/wifi_conection_scaner/features/coordinate_manager/data/WifiScanner.kt
+++ app/src/main/java/com/example/wifi_conection_scaner/features/coordinate_manager/data/WifiScanner.kt
@@ -1,2 +1,21 @@
 package com.example.wifi_conection_scaner.features.coordinate_manager.data
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+class WifiScanner(private val context: Context) {
+
+    private val connectivityManager =
+        context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun getCurrentRssi(): Int {
+        val network = connectivityManager.activeNetwork
+        val capabilities = connectivityManager.getNetworkCapabilities(network)
+        val rssi = capabilities?.signalStrength
+
+        return rssi ?: -100
+    }
+}
